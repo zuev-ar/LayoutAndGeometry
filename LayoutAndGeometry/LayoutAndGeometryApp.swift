5db4ff497ea032f508c0a26420f87797de981968
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-10-05 18:03:17 +0500

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Arkasha Zuev on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
